a0aed9a00b8f79b3f71fd277707a7f4e4aad1380
Aditya Mane <83772267+aditya711-code@users.noreply.github.com>
2022-04-12 07:58:12 +0530

    Create midoftwopointers.cpp

---
+++ Day20/midoftwopointers.cpp
@@ -0,0 +1,12 @@
+ListNode* middleNode(ListNode* head) {
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+        
+        
+    }
+       //slow and fast pointer
