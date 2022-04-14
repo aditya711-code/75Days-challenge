bd34673e905a886d5a57de8f965f8ed5a7f7f2d4
Aditya Mane <83772267+aditya711-code@users.noreply.github.com>
2022-04-14 08:27:13 +0530

    Create Detectcycleremoveloop.cpp

---
+++ Day22/Detectcycleremoveloop.cpp
@@ -0,0 +1,23 @@
+ListNode *detectCycle(ListNode *head) {
+      if(head==NULL || head->next==NULL)
+            return NULL;
+       ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* entry=head;
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                while(entry!=slow)
+                {
+                    entry=entry->next;
+                    slow=slow->next;
+                    
+                }
+                return entry;
+            }
+        }
+        return NULL;
+    }
