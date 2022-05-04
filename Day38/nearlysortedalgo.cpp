28fbef11adce98cf276917aa3fd432428dbdd039
Aditya Mane <83772267+aditya711-code@users.noreply.github.com>
2022-05-04 09:18:56 +0530

    Update nearlysortedalgo.cpp

--- Day38/nearlysortedalgo.cpp
+++ Day38/nearlysortedalgo.cpp
@@ -15,3 +15,31 @@ long long required(long long arr[], long long n, long long k) {
         }
         return ans*-1;
     }
+//THr real problem of nearly sorted or K sorted array
+/*Input : arr[] = {6, 5, 3, 2, 8, 10, 9}
+            k = 3 
+Output : arr[] = {2, 3, 5, 6, 8, 9, 10}
+*/
+vector<int>sortedArray(vector<int>&nums,int k)
+{
+        int n=nums.size();
+        int size;
+        size=(n==k)?k:k+1;
+        priority_queue<int,vector<int>,greater<int>>pqmin(nums.begin(),nums.begin()+size);
+        int index=0;
+        vector<int>output(n,0);
+        for(int i=k+1;i<n;i++)
+        {
+                output[index]=pqmin.top();
+                pqmin.pop();
+                pq.push(nums[i]);
+                index++;
+        }
+        while(!pqmin.empty())
+        {
+                output[index]=pqmin.top();
+                pqmin.pop():
+                index++;
+        }
+        return output;
+}
