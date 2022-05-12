e91bfa413d25e6c41adc55b429a043df91652e90
Aditya Mane <83772267+aditya711-code@users.noreply.github.com>
2022-05-12 19:20:02 +0530

    Create lcs.cpp

---
+++ Day48/lcs.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size();
+        int n=text2.size();
+         int **output = new int *[m + 1];
+    for (int i = 0; i <= m; i++)
+    {
+        output[i] = new int[n + 1];
+    }
+    for (int i = 0; i <= m; i++)
+    {
+        output[i][0] = 0;
+    }
+    for (int j = 0; j <= n; j++)
+    {
+        output[0][j] = 0;
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (text1[m - i] == text2[n - j])
+            {
+                output[i][j] = 1 + output[i - 1][j - 1];
+            }
+            else
+            {
+                int a = output[i - 1][j];
+                int b = output[i][j - 1];
+               
+                output[i][j] = max(a, b);
+            }
+        }
+    }
+    return output[m][n];
+        
+    }
+};
