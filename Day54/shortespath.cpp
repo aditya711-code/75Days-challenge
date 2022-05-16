afa27361729d299b0ea7e14bca29d796b426108f
Aditya Mane <83772267+aditya711-code@users.noreply.github.com>
2022-05-16 10:05:29 +0530

    Create shortespath.cpp

---
+++ Day54/shortespath.cpp
@@ -0,0 +1,13 @@
+int minimumStep(int n){
+        //complete the function here
+        if(n==1)return 0;
+        if(n%3==0)
+        {
+            return 1+minimumStep(n/3);
+        }
+        else
+        {
+            return 1+minimumStep(n-1);
+        }
+       
+    }
